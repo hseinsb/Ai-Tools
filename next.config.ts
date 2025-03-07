bfe306dbb09e7d25d16aeaf54e2411f4c8c7e525
hseinsb <hussein.sbeiti.wb@gmail.com>
2025-03-07 00:38:52 -0500

    Add your commit message here

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['undici', 'firebase'],
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack: (config: { module: { rules: { test: RegExp; use: { loader: string; options: { presets: string[]; plugins: (string | { loose: boolean; })[][]; }; }; }[]; }; }) => {
     // Add specific rule for undici
     config.module.rules.push({
